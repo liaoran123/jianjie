7dc6b781397686cf2758e16c285e9cf543b688fd
liaoran123 <2065551801@qq.com>
2023-06-09 10:58:10 +0800

    1

--- xbdb/opendb.go
+++ xbdb/opendb.go
@@ -1,5 +1,5 @@
-//小白数据库
-//表信息
+// 小白数据库
+// 表信息
 package xbdb
 
 import (
@@ -12,24 +12,46 @@ import (
 
 var xb *leveldb.DB
 
-//创建或打开数据库
+// 创建或打开数据库
 func OpenDb(fp string) error {
 	xb, err = leveldb.OpenFile(fp+"db", nil)
 	return err
 }
 
-//创建所有表的操作结构
+// 创建所有表的操作结构
 func OpenTableStructs() map[string]*Table {
-	iter := xb.NewIterator(util.BytesPrefix([]byte(Tbspfx+Split)), nil)
 	Tables := make(map[string]*Table)
+	tbnames := GetTbnames()
+	for _, v := range tbnames {
+		Tables[v] = NewTable(xb, v)
+	}
+	return Tables
+	/*
+		iter := xb.NewIterator(util.BytesPrefix([]byte(Tbspfx+Split)), nil)
+		Tables := make(map[string]*Table)
+		tbname := ""
+		for iter.Next() {
+			tbname = strings.Split(string(iter.Key()), Split)[1]
+			Tables[tbname] = NewTable(tbname)
+		}
+		iter.Release()
+		if iter.Error() != nil {
+			fmt.Printf("iter.Error(): %v\n", iter.Error())
+		}*/
+
+}
+
+// 获取数据库所有的表名称
+func GetTbnames() (r []string) {
+	iter := xb.NewIterator(util.BytesPrefix([]byte(Tbspfx+Split)), nil)
 	tbname := ""
 	for iter.Next() {
 		tbname = strings.Split(string(iter.Key()), Split)[1]
-		Tables[tbname] = NewTable(tbname)
+		r = append(r, tbname)
 	}
 	iter.Release()
 	if iter.Error() != nil {
 		fmt.Printf("iter.Error(): %v\n", iter.Error())
 	}
-	return Tables
+	return r
 }
